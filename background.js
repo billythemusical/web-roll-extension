d09914029390ba6be91762306d2f5d10951403ff
Billy Bennett <bennett.billy@gmail.com>
2023-02-08 15:53:23 -0500

    version-3 replace

--- background.js
+++ background.js
@@ -1,10 +1,28 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log('got a message')
-    if(request.message === "start_scroll") {
-        console.log('got a message')
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(tabs[0].id, {file: "content.js"});
-        });
-    }
-});
+let speed = 4
+
+async function getTabId() {
+    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true}) 
+    // console.log(`got tabId the first time: ${tab.id}`)
+    return tab.id
+}
+
+chrome.runtime.onMessage.addListener(handleStart)
+
+async function handleStart(request, sender, sendResponse) {
+        if(request.message === "start_scroll") {
+    
+            const tabId = await getTabId()
+            console.log(`got the new tab id: ${tabId}`) 
+    
+            const response = await chrome.tabs.sendMessage(tabId, { speed: request.speed })  
+            console.log(`sent the speed: ${request.speed} w ${response.reply}`)
+    
+        }
+}
+
+// chrome.scripting
+//             .executeScript({
+//                 target: { tabId: tabId },
+//                 files: ["scroll.js"]
+//             })
+//         .then(()=>console.log("ran the scroll I think"))
--- content.js
+++ content.js
@@ -1,41 +1,53 @@
+console.log("running JS content")
 
-(function() {
-    console.log("running content")
-    var interval = 4
-    var increment = 2
-    var intervalObj = null;
-    var retry = 0;
-    var clickHandler = function() { 
-        console.log("Clicked; stopping autoscroll");
-        clearInterval(intervalObj);
-        document.body.removeEventListener("click", clickHandler);
-    }
-    function scrollDown() { 
-        var scrollHeight = document.body.scrollHeight,
-            scrollTop = document.body.scrollTop,
-            innerHeight = window.innerHeight,
-            difference = (scrollHeight - scrollTop) - innerHeight
+let speed = 4
+let increment = 2
+let intervalObj = null;
+let retry = 0;
 
-        if (difference > 0) { 
-            window.scrollBy(0, increment);
-            if (retry > 0) { 
-                retry = 0;
-            }
-            console.log("scrolling down more");
+// Here we listen for a message to execute the scrolling
+chrome.runtime.onMessage.addListener(handleRequest)
+
+async function handleRequest(message, sender, sendResponse) {
+        if(message.speed) {
+            speed = message.speed
+            console.log("speed value updated", speed)
+            sendResponse({reply: "speed received"})
+        }
+        if(message.start) {
+            document.body.addEventListener("click", clickHandler);
+            intervalObj = setInterval(scrollDown, speed);
+            sendResponse({reply: "start received by scroll.js"})
+        }
+}
+
+
+function scrollDown() { 
+    let scrollHeight = document.body.scrollHeight,
+        scrollTop = document.body.scrollTop,
+        innerHeight = window.innerHeight,
+        difference = (scrollHeight - scrollTop) - innerHeight
+
+    if (difference > 0) { 
+        window.scrollBy(0, increment);
+        if (retry > 0) { 
+            retry = 0;
+        }
+        console.log("scrolling down more");
+    } else {
+        if (retry >= 3) {
+            console.log("reached bottom of page; stopping");
+            clearInterval(intervalObj);
+            document.body.removeEventListener("click", clickHandler);
         } else {
-            if (retry >= 3) {
-                console.log("reached bottom of page; stopping");
-                clearInterval(intervalObj);
-                document.body.removeEventListener("click", clickHandler);
-            } else {
-                console.log("[apparenty] hit bottom of page; retrying: " + (retry + 1));
-                retry++;
-            }
+            console.log("[apparenty] hit bottom of page; retrying: " + (retry + 1));
+            retry++;
         }
     }
+}
 
-    document.body.addEventListener("click", clickHandler);
-
-    intervalObj = setInterval(scrollDown, interval);
-    
-})()
+function clickHandler () { 
+    console.log("Clicked; stopping autoscroll");
+    clearInterval(intervalObj);
+    document.body.removeEventListener("click", clickHandler);
+}
